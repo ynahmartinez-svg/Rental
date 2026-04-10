dc4caf00e6665da3f363a71a0441b2233f3b5539
zoweyyy <zblaguardia0326@gmail.com>
2026-04-11 00:17:53 +0800

    combo box and button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,38 +36,44 @@
             comboBox1 = new ComboBox();
             button2 = new Button();
             dateTimePicker1 = new DateTimePicker();
+            button3 = new Button();
+            comboBox2 = new ComboBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(309, 20);
+            label1.Location = new Point(386, 25);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(118, 20);
+            label1.Size = new Size(142, 25);
             label1.TabIndex = 0;
             label1.Text = "RENTAL SYSTEM";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(53, 73);
+            label2.Location = new Point(66, 91);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(52, 20);
+            label2.Size = new Size(63, 25);
             label2.TabIndex = 1;
             label2.Text = "Name:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(119, 73);
+            textBox1.Location = new Point(149, 91);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(155, 31);
             textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            button1.Location = new Point(85, 121);
+            button1.Location = new Point(106, 151);
+            button1.Margin = new Padding(4, 4, 4, 4);
             button1.Name = "button1";
-            button1.Size = new Size(169, 43);
+            button1.Size = new Size(211, 54);
             button1.TabIndex = 3;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
@@ -75,9 +81,10 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(477, 132);
+            label3.Location = new Point(596, 165);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(59, 25);
             label3.TabIndex = 4;
             label3.Text = "label3";
             label3.Click += label3_Click;
@@ -85,34 +92,54 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(424, 172);
-            comboBox1.Margin = new Padding(2, 3, 2, 3);
+            comboBox1.Location = new Point(530, 215);
+            comboBox1.Margin = new Padding(2, 4, 2, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(195, 28);
+            comboBox1.Size = new Size(243, 33);
             comboBox1.TabIndex = 5;
             // 
             // button2
             // 
-            button2.Location = new Point(458, 244);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(576, 333);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(86, 31);
+            button2.Size = new Size(108, 39);
             button2.TabIndex = 6;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(64, 268);
+            dateTimePicker1.Location = new Point(80, 335);
+            dateTimePicker1.Margin = new Padding(4, 4, 4, 4);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(333, 27);
+            dateTimePicker1.Size = new Size(415, 31);
             dateTimePicker1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(576, 393);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 8;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(524, 264);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(249, 33);
+            comboBox2.TabIndex = 9;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 449);
+            ClientSize = new Size(1000, 561);
+            Controls.Add(comboBox2);
+            Controls.Add(button3);
             Controls.Add(dateTimePicker1);
             Controls.Add(button2);
             Controls.Add(comboBox1);
@@ -121,6 +148,7 @@
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -137,5 +165,7 @@
         private ComboBox comboBox1;
         private Button button2;
         private DateTimePicker dateTimePicker1;
+        private Button button3;
+        private ComboBox comboBox2;
     }
 }
